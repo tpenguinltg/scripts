e14a3ffa9744b2f19524080cc7bdb6dc1fc953c0
tPenguinLTG <tpenguinltg@disroot.org>
2020-08-29 17:05:43 -0400

    [backup.sh] Encrypt the backups
    The script asks for a passphrase and attempts to decrypt a
    passphrase_check file in the backup directory. If it decrypts
    successfully, it proceeds with the backup.
    
    The passphrase is given as a herestring to FD 3. The shebang has been
    changed to a bash shebang because herestrings are a bashism.

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-destination="/mnt/backup/jason"
+destination="/mnt/backup"
+passphrase_check_file="${destination}/passphrase_check"
 
 # needs root privileges to run
 if [ $(id -u) -ne 0 ]; then
@@ -17,6 +18,32 @@ if [ ! -d "$destination" ]; then
   exit 1
 fi
 
+if [ -z "$PASSPHRASE" ]; then
+  stty -echo
+  printf 'Passphrase: '
+  IFS= read -r PASSPHRASE
+  export PASSPHRASE
+  echo
+
+  if ! [ -r "${passphrase_check_file}" ]; then
+    printf 'Passphrase (confirm): '
+    IFS= read -r confirm_passphrase
+    echo
+
+    if [ "$PASSPHRASE" != "$confirm_passphrase" ]; then
+      echo "ERROR: Passphrases do not match. Aborting." >&2
+      stty echo
+      exit 1
+    fi
+    unset confirm_passphrase
+
+    </dev/urandom tr -dc 'A-Za-z0-9' | gpg --batch --symmetric --passphrase-fd 3 -o "${passphrase_check_file}" 3<<<"$PASSPHRASE"
+  fi
+  stty echo
+fi
+
+gpg --batch --decrypt --passphrase-fd 3 "${passphrase_check_file}" 3<<<"$PASSPHRASE" >/dev/null 2>&1 || { echo "ERROR: Incorrect passphrase." >&2; exit 1; }
+
 exit_status=
 
 # bit field:
@@ -28,7 +55,7 @@ failed=0
 # System files
 echo Backing up system files...
 [ -d "$destination/system" ] || mkdir "$destination/system"
-duplicity --full-if-older-than 3M --verbosity info --no-encryption --asynchronous-upload / --exclude-if-present .nobackup --include /etc --include /var/lib/pacman/local --include /boot --exclude '**' "file://$destination/system"
+duplicity --full-if-older-than 3M --verbosity info --asynchronous-upload / --exclude-if-present .nobackup --include /etc --include /var/lib/pacman/local --include /boot --exclude '**' "file://$destination/system"
 exit_status=$?
 if [ $exit_status -eq 0 ]; then
   echo System files backup complete.
@@ -51,7 +78,7 @@ sync
 # crontabs in /etc are already backed up in system backup
 echo Backing up user crontabs...
 [ -d "$destination/crontabs" ] || mkdir "$destination/crontabs"
-duplicity --full-if-older-than 3M --verbosity info --no-encryption --asynchronous-upload / --exclude-if-present .nobackup --include /etc --include /var/spool/cron --exclude '**' "file://$destination/crontabs"
+duplicity --full-if-older-than 3M --verbosity info --asynchronous-upload / --exclude-if-present .nobackup --include /etc --include /var/spool/cron --exclude '**' "file://$destination/crontabs"
 exit_status=$?
 if [ $exit_status -eq 0 ]; then
   echo User crontab backup complete.
@@ -73,7 +100,7 @@ sync
 # User data
 echo Backing up user data...
 [ -d "$destination/data" ] || mkdir "$destination/data"
-duplicity --full-if-older-than 3M --verbosity info --no-encryption --asynchronous-upload / --exclude-if-present .nobackup --include /home --include /data --exclude '**' "file://$destination/data"
+duplicity --full-if-older-than 3M --verbosity info --asynchronous-upload / --exclude-if-present .nobackup --include /home --exclude '**' --exclude-other-filesystems "file://$destination/data"
 exit_status=$?
 if [ $exit_status -eq 0 ]; then
   echo User data backup complete.
@@ -93,4 +120,5 @@ fi
 sync
 
 echo Backup complete.
+unset PASSPHRASE
 exit $failed
